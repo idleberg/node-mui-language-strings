92322447aeb7312125f50339153d1bc82aa0db4f
idleberg <git@idleberg.com>
2019-04-13 00:51:19 +0200

    capture language names

--- dist/index.js
+++ dist/index.js
@@ -7,17 +7,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 var parse = function (input, options) {
     if (options === void 0) { options = {}; }
-    var output = {};
+    var output = {
+        file: null,
+        english: null,
+        native: null,
+        nativeASCII: null,
+        strings: {}
+    };
     try {
         var lines = input.split('\n');
         lines.forEach(function (line, index) {
             line = line.replace(/(#.*)$/mg, '');
             line = line.trim();
+            if (line.startsWith('!insertmacro LANGFILE')) {
+                var matches_1 = line.match(/"[^"]*"|=/g);
+                matches_1.forEach(function (match, index) {
+                    // Strip quotes
+                    matches_1[index] = match.replace(/^"/, '').replace(/"$/, '');
+                    if (match === '=') {
+                        matches_1[index] = matches_1[index - 1];
+                    }
+                });
+                output['file'] = matches_1[0];
+                output['english'] = matches_1[1];
+                output['native'] = matches_1[2];
+                output['nativeASCII'] = matches_1[3];
+            }
             if (line.startsWith('${LangFileString}')) {
                 var re = /^\${LangFileString}\s*(?<key>\w+)\s*\"(?<value>.*)\"$/;
                 // @ts-ignore
                 var groups = re.exec(line).groups;
-                output[groups.key] = groups.value;
+                output.strings[groups.key] = groups.value;
             }
         });
     }
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,13 @@
  * @returns - MUI object
  */
 const parse = (input: string, options: ParserOptions = {}): Object|string => {
-  let output: any = {};
+  let output: any = {
+    file: null,
+    english: null,
+    native: null,
+    nativeASCII: null,
+    strings: {}
+  };
 
   try {
     const lines = input.split('\n');
@@ -13,12 +19,30 @@ const parse = (input: string, options: ParserOptions = {}): Object|string => {
         line = line.replace(/(#.*)$/mg, '');
         line = line.trim();
 
+        if (line.startsWith('!insertmacro LANGFILE')) {
+          let matches = line.match(/"[^"]*"|=/g);
+
+          matches.forEach( (match, index) => {
+            // Strip quotes
+            matches[index] = match.replace(/^"/, '').replace(/"$/, '');
+
+            if (match === '=') {
+              matches[index] = matches[index - 1];
+            }
+          });
+
+          output['file'] = matches[0];
+          output['english'] = matches[1];
+          output['native'] = matches[2];
+          output['nativeASCII'] = matches[3];
+        }
+
         if (line.startsWith('${LangFileString}')) {
             const re = /^\${LangFileString}\s*(?<key>\w+)\s*\"(?<value>.*)\"$/;
             // @ts-ignore
             const { groups } = re.exec(line);
 
-            output[groups.key] = groups.value;
+            output.strings[groups.key] = groups.value;
         }
     });
   } catch (e) {
